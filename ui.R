5b93f614579c9e81976e145ba3892016dc7933c5
DDelbende <damien.delbende@gmail.com>
2017-06-13 09:08:00 +0200

    modif mineures

--- ui.R
+++ ui.R
@@ -12,7 +12,6 @@ shinyUI(fluidPage(
   # Application title
   titlePanel("Old Faithful Geyser Data"),
 
-  # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
       sliderInput("bins",
